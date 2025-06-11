b62e5fb75b3846de6a97bbc930d0ed66b4631baa
felipebel25 <felipemedina.developer@gmail.com>
2025-06-10 23:19:48 -0500

    test case

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -2,9 +2,9 @@ import { Box } from '@mui/material'
 
 import { styles } from './stylesPortfolio'
 import 'animate.css'
-import { HeaderHomePortfolio } from 'components/molecules/homePortfolio/header/headerHomePortfolio'
 import { MainHomePortfolio } from 'components/molecules/homePortfolio/main/mainHomePortfolio'
 import { FooterPortfolio } from 'components/molecules/homePortfolio/footer/footerPortfolio'
+import { HeaderHomePortfolio } from 'components/molecules/homePortfolio/header/headerHomePortfolio'
 
 export const Portfolio = () => (
     <Box sx={styles.main}>
